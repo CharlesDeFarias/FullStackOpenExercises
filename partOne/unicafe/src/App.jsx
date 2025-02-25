bd21553d633f74224a5ede9e6454c5d80aa11d8d
Charles De Farias <cddefari@gmail.com>
2025-02-25 14:33:08 -0500

    completed exercise 1.1

---
+++ partOne/unicafe/src/App.jsx
@@ -0,0 +1,54 @@
+//consists of three new components: Header, Content, and Total. All data still resides in the App component, which passes the necessary data to each component using props. 
+// Header takes care of rendering the name of the course, 
+// Content renders the parts and their number of exercises and 
+// Total renders the total number of exercises.
+const App = () => {
+ 
+  const course ={
+    title: 'Half Stack application development',
+    parts: [{
+      number: 1,
+      title: 'Fundamentals of React',
+      exerciseCount: 10,
+      },{
+        number: 2,
+        title: 'Using props to pass data',
+        exerciseCount: 7
+      },{
+        number: 3,
+        title: 'State of a component',
+        exerciseCount: 14
+      }],
+  }
+
+  const Header = ({courseTitle}) => <h1>{courseTitle}</h1>
+
+  const Content = ({parts}) => {
+    console.log(parts)
+    return (
+      <>
+        <h3>Parts:</h3>
+        <ul>
+          {parts.map((part) => <li key={part.number}> Part: {part.number} - {part.title} - Exercises: {part.exerciseCount}</li>)}
+        </ul>
+      </>
+    )
+  }
+
+  const Total = ({parts}) => {
+    return (
+      <p>Total number of exercises: {parts.reduce((acc, part) => acc + part.exerciseCount, 0)}</p>
+    )
+  }
+  console.log(course.title)
+
+  return (
+    <div>
+      <Header courseTitle={course.title} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
