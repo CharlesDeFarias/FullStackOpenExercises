d67ab20de5fef6ceac65dfd5ee204ce751719321
Charles De Farias <cddefari@gmail.com>
2025-02-26 13:40:03 -0500

    finished 2.8

--- partTwo/phonebook/src/App.jsx
+++ partTwo/phonebook/src/App.jsx
@@ -2,19 +2,28 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      phone: "555-555-5555"
+    },
   ]) 
 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleName = (e) => {
     setNewName(e.target.value)
   }
 
+  const handleNumber = (e) => {
+    setNewNumber(e.target.value)
+  }
+
+
   const handleButton = (e) => {
     e.preventDefault();
     let isRepeat = persons.find(person => person.name === newName)
-    console.log(isRepeat)
+  
     isRepeat ? alert("This name has already been saved. If you need to repeat a name, change it slightly to submit.") : setPersons(persons.concat({name: newName}))
     console.log(newName)
     console.log(persons)
@@ -27,13 +36,14 @@ const App = () => {
       <form>
         <div>
           name: <input value={newName} onChange={handleName} />
+          Number: <input value={newNumber} onChange={handleNumber} />
         </div>
         <div>
           <button type="submit" onClick={handleButton}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div></div>
+      <div>{persons.map((person)=> <li key={person.name}>{person.name}, {person.phone}</li>)}</div>
     </div>
   )
 }
