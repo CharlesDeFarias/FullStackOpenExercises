4327a8af37d9c3fb453f69ccd537bcd59021e4a9
Charles De Farias <cddefari@gmail.com>
2025-02-26 09:50:04 -0500

    finished 1.11

--- partOne/unicafe/src/App.jsx
+++ partOne/unicafe/src/App.jsx
@@ -3,15 +3,71 @@
 // The application must display the total number of collected feedback for each category
 import { useState } from 'react'
 
+const StatisticsLine = ({stat, value}) => {
+  console.log(stat)
+  console.log(value)
+   if(stat === "positive") {
+    stat= "Positive %"
+  }
+
+  return (
+      <li key={value}>{stat}: {value}</li>
+    )
+}
+
+const Statistics = ({votes}) => { 
+  const {good, neutral, bad} = votes;
+  let total = good + neutral + bad;
+  let displayComponent = total > 0;
+
+  let avg = displayComponent ? (good - bad) / (total) : 0
+  let positive = displayComponent ? (good / (total)) * 100 : 0
+  
+  const values = {
+    good: good,
+    neutral: neutral,
+    bad: bad,
+    total: total,
+    average: avg,
+    positive: positive
+  }
+
+  const keys = Object.keys(values);
+
+  if(displayComponent) {
+    return (
+    <>
+      <h2>Statistics</h2>
+      {keys.map((key) => <StatisticsLine key={key} stat={key} value={values[key]} />)}
+    </>
+    )}else{
+    return (
+    <>
+      <h2>Statistics</h2>
+      <p>No feedback given</p>
+    </>
+  )}
+}
+
+const FeedbackBttn = ({value, handleButton}) => {
+  return (
+    <button value={value} onClick={handleButton} key={value}>{value}</button>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const values = { 
+    good: good,
+    neutral: neutral,
+    bad: bad,
+  }
 
   const handleButton = (e) => {
     const reaction = e.target.value;
-    console.log(reaction)
     if (reaction === "good") {
       setGood(good + 1)
     } else if (reaction === "neutral") {
@@ -24,16 +80,10 @@ const App = () => {
   return (
     <div>
       <h2>Give Feedback</h2>
-      <button value={"good"} onClick={handleButton}>Good</button>
-      <button value={"neutral"} onClick={handleButton}>Neutral</button>
-      <button value={"bad"} onClick={handleButton}>Bad</button>
-      <h2>Statistics</h2>
-      <li>Good: {good}</li>
-      <li>Neutral: {neutral}</li>
-      <li>Bad: {bad}</li>
-      <li>Total: {good + neutral + bad}</li>
-      <li>Average: {(good - bad) / (good + neutral + bad)}</li>
-      <li>Positive %: {(good / (good + neutral + bad)) * 100}%</li>
+      <FeedbackBttn value={"good"} handleButton={handleButton} />
+      <FeedbackBttn value={"neutral"} handleButton={handleButton} />
+      <FeedbackBttn value={"bad"} handleButton={handleButton} />
+      <Statistics votes={values}/>
     </div>
   )
 }
